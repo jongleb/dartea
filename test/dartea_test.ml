a40be78a352e54f5e3b76814fac9f82968ebea89
Gleb Patcia <jongleb@yandex.ru>
2023-10-28 12:38:41 +0300

    add a few tests

--- test/dartea_test.ml
+++ test/dartea_test.ml
@@ -408,6 +408,133 @@ let test_ty_alias_record_with_params_row_type _ =
   let result = Parser.prog Lexer.token (Lexing.from_string input) in
   assert_equal expect_data result
 
+let test_function_types _ =
+  let expect_data =
+    [
+      Type_alias
+        {
+          data =
+            {
+              params = [];
+              content =
+                Function
+                  {
+                    arguments =
+                      [
+                        { params = []; content = Type_var "a" };
+                        { params = []; content = Type_var "a" };
+                      ];
+                  };
+            };
+          params = [ "a" ];
+          id = "Id";
+        };
+    ]
+  in
+  let input = "type alias Id a = a -> a" in
+  let result = Parser.prog Lexer.token (Lexing.from_string input) in
+  assert_equal expect_data result
+
+let test_function_with_function_param_types _ =
+  let expect_data =
+    [
+      Type_alias
+        {
+          data =
+            {
+              params = [];
+              content =
+                Function
+                  {
+                    arguments =
+                      [
+                        { params = []; content = Concrete "String" };
+                        { params = []; content = Concrete "Int" };
+                        {
+                          params = [];
+                          content =
+                            Tuples
+                              [
+                                { params = []; content = Type_var "a" };
+                                { params = []; content = Type_var "b" };
+                              ];
+                        };
+                        {
+                          params = [];
+                          content =
+                            Function
+                              {
+                                arguments =
+                                  [
+                                    { params = []; content = Concrete "String" };
+                                    { params = []; content = Type_var "a" };
+                                    {
+                                      params = [];
+                                      content =
+                                        Tuples
+                                          [
+                                            {
+                                              params = [];
+                                              content = Type_var "b";
+                                            };
+                                            {
+                                              params = [];
+                                              content = Type_var "b";
+                                            };
+                                            {
+                                              params = [];
+                                              content = Type_var "c";
+                                            };
+                                          ];
+                                    };
+                                  ];
+                              };
+                        };
+                      ];
+                  };
+            };
+          params = [ "a"; "b"; "c" ];
+          id = "FunName";
+        };
+    ]
+  in
+  let input =
+    "type alias FunName a b c = String -> Int -> (a, b) -> (String -> a -> (b, \
+     b, c))"
+  in
+  let result = Parser.prog Lexer.token (Lexing.from_string input) in
+  assert_equal expect_data result
+
+let test_any_fail_if_type_parametr_is_uppecase _ =
+  let input = "type alias Id A = A -> A" in
+  let res =
+    try
+      let _ = Parser.prog Lexer.token (Lexing.from_string input) in
+      true
+    with _ -> false
+  in
+  assert_equal false res
+
+let test_any_fail_if_ty_alias_name_is_lowercase _ =
+  let input = "type alias anyName a = a -> a" in
+  let res =
+    try
+      let _ = Parser.prog Lexer.token (Lexing.from_string input) in
+      true
+    with _ -> false
+  in
+  assert_equal false res
+
+let test_any_fail_if_any_char_in_middle_of_valid_code _ =
+  let input = "type alias ValidName - a = a -> a" in
+  let res =
+    try
+      let _ = Parser.prog Lexer.token (Lexing.from_string input) in
+      true
+    with _ -> false
+  in
+  assert_equal false res
+
 let suite =
   "Dartea_test"
   >::: [
@@ -424,6 +551,15 @@ let suite =
          >:: test_ty_alias_and_record_and_plain_and_one_more_tuple;
          "test_ty_alias_record_with_params_row_type"
          >:: test_ty_alias_record_with_params_row_type;
+         "test_function_types" >:: test_function_types;
+         "test_function_with_function_param_types"
+         >:: test_function_with_function_param_types;
+         "test_fail_if_type_parametr_is_uppecase"
+         >:: test_any_fail_if_type_parametr_is_uppecase;
+         "test_any_fail_if_ty_alias_name_is_lowercase"
+         >:: test_any_fail_if_ty_alias_name_is_lowercase;
+         "test_any_fail_if_any_char_in_middle_of_valid_code"
+         >:: test_any_fail_if_any_char_in_middle_of_valid_code;
        ]
 
 let () = run_test_tt_main suite
