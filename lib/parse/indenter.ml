217959f811128b9ddf151d94d35b4fa77bc45b57
Gleb Patcia <jongleb@yandex.ru>
2025-01-13 00:07:38 +0300

    wip

--- lib/parse/indenter.ml
+++ lib/parse/indenter.ml
@@ -1,54 +1,5 @@
 open Parser
 
-let show_token token =
-  match token with
-  | TYPE -> "TYPE"
-  | ALIAS -> "ALIAS"
-  | LCNAME name -> Printf.sprintf "LCNAME(%s)" name
-  | UCNAME name -> Printf.sprintf "UCNAME(%s)" name
-  | UCNAME_PATH path -> Printf.sprintf "UCNAME_PATH(%s)" path
-  | ACCESSOR accessor -> Printf.sprintf "ACCESSOR(%s)" accessor
-  | INT value -> Printf.sprintf "INT(%d)" value
-  | STRING str -> Printf.sprintf "STRING(%s)" str
-  | FLOAT value -> Printf.sprintf "FLOAT(%f)" value
-  | EQUAL -> "="
-  | EOF -> "EOF"
-  | LPAREN -> "LPAREN"
-  | RPAREN -> "RPAREN"
-  | LBRACE -> "LBRACE"
-  | RBRACE -> "RBRACE"
-  | COMMA -> "COMMA"
-  | COLON -> "COLON"
-  | PIPE -> "PIPE"
-  | ARROW -> "ARROW"
-  | LBRACKET -> "LBRACKET"
-  | RBRACKET -> "RBRACKET"
-  | LET -> "LET"
-  | IN -> "IN"
-  | PLUS -> "PLUS"
-  | MINUS -> "MINUS"
-  | TIMES -> "TIMES"
-  | DIV -> "DIV"
-  | IF -> "IF"
-  | THEN -> "THEN"
-  | ELSE -> "ELSE"
-  | CASE -> "CASE"
-  | OF -> "OF"
-  | WILDCARD -> "WILDCARD"
-  | CONS -> "CONS"
-  | UNIT -> "UNIT"
-  | IMPORT -> "IMPORT"
-  | AS -> "AS"
-  | DOT -> "DOT"
-  | TWO_DOTS -> "TWO_DOTS"
-  | EXPOSING -> "EXPOSING"
-  | EQ_EQ -> "EQ_EQ"
-  | GT -> "GT"
-  | LT -> "LT"
-  | MODULE -> "MODULE"
-  | INDENT -> "INDENT"
-  | DEDENT -> "DEDENT"
-
 type indent_context =
   | Let
   | Let_def
@@ -78,7 +29,6 @@ let state =
     }
   in
   Stack.push (0, Top_level) state.stack;
-  (* Кладем начальное состояние как пару *)
   state
 
 let close_one token lexbuf =
@@ -89,67 +39,33 @@ let close_one token lexbuf =
     ignore @@ Stack.pop state.stack;
     DEDENT)
 
-(* Helper functions for indentation handling *)
-let push_indent level context =
-  Stack.push (level, context) state.stack;
+let push_indent level context = Stack.push (level, context) state.stack
 
-  prerr_endline
-  @@ Printf.sprintf "push_indent, NOW %s"
-       (show_indent_context @@ snd @@ Stack.top @@ state.stack)
-
-(* И добавим функцию для получения текущего контекста *)
 let get_current_context () =
   match Stack.top_opt state.stack with
   | Some (_, ctx) -> ctx
   | None -> Top_level
 
 let get_current_indent () =
-  match Stack.top_opt state.stack with
-  | Some (level, _) -> level (* Берем только уровень отступа из пары *)
-  | None -> 0
+  match Stack.top_opt state.stack with Some (level, _) -> level | None -> 0
 
 let close_until ident next_token =
-  prerr_endline @@ Printf.sprintf "close_until ident %n " ident;
   let rec go () =
     match Stack.pop_opt state.stack with
-    | None ->
-        prerr_endline "Stack.pop_opt -> None";
-        Stack.push (0, Top_level) state.stack
-    | Some ((_, Let) as x) ->
-        prerr_endline "Some ((_, Let) as x)";
-        Stack.push x state.stack
+    | None -> Stack.push (0, Top_level) state.stack
+    | Some ((_, Let) as x) -> Stack.push x state.stack
     | Some ((_, Let_inline) as x) ->
-        prerr_endline "Some ((_, Let_inline) as x)";
         Stack.push x state.stack;
         Queue.add DEDENT state.queue
     | Some (ident', ctx) when ident < ident' ->
-        prerr_endline
-        @@ Printf.sprintf
-             "Stack.pop_opt -> Some (ident', _) when ident < ident', ident: \
-              %n, ident' : %n , ctx: %s"
-             ident ident' (show_indent_context ctx);
         Queue.add DEDENT state.queue;
         go ()
     | Some (ident', ctx) when ident > ident' ->
-        prerr_endline "push back";
         Stack.push (ident', ctx) state.stack
-    | Some (ident', ctx) ->
-        prerr_endline
-        @@ Printf.sprintf "Some (ident', ctx), ident: %n, ident' : %n , ctx: %s"
-             ident ident' (show_indent_context ctx);
-        ()
+    | Some (ident', ctx) -> ()
   in
   go ();
-  if Queue.is_empty state.queue then (
-    prerr_endline "close_until, branch queue is empty";
-    next_token ())
-  else (
-    prerr_endline
-    @@ Printf.sprintf "Queue lenght is %n " (Queue.length state.queue);
-    let result = Queue.take state.queue in
-    prerr_endline
-    @@ Printf.sprintf "close_until, queue isn't empty %s" (show_token result);
-    result)
+  if Queue.is_empty state.queue then next_token () else Queue.take state.queue
 
 let count_indent str =
   str |> String.to_seq |> List.of_seq
@@ -160,82 +76,48 @@ let count_indent str =
 let handle_newline nl token lexbuf =
   let indent_level = count_indent nl in
 
-  prerr_endline @@ Printf.sprintf "handle_newline, indent level %n" indent_level;
-
   let pos = lexbuf.Lexing.lex_curr_p in
 
   state.current_string_cnum <- pos.Lexing.pos_cnum - count_indent nl;
 
   let rec go () =
-    prerr_endline "Go called";
     let current = get_current_indent () in
     let next_token () = token lexbuf in
 
     state.ident_compare <- Int.compare indent_level current;
 
-    prerr_endline @@ Printf.sprintf "Current indent context value %n" current;
-    prerr_endline
-    @@ Printf.sprintf "indent_level < current, %b" (indent_level < current);
-
     match get_current_context () with
-    | Top_level when indent_level = 0 ->
-        prerr_endline "Top_level when indent_level = 0";
-        token lexbuf
-    | Top_level ->
-        prerr_endline "Top_level";
-        close_until indent_level next_token
-    | Expression when indent_level < current ->
-        prerr_endline "Expression when indent_level < current";
-        close_one token lexbuf
-    | Expression ->
-        prerr_endline "Expression";
-        token lexbuf
+    | Top_level when indent_level = 0 -> token lexbuf
+    | Top_level -> close_until indent_level next_token
+    | Expression when indent_level < current -> close_one token lexbuf
+    | Expression -> token lexbuf
     | (Case | Let) when indent_level > current ->
-        prerr_endline "Case, indent_level > current";
         let _, l = Stack.pop state.stack in
         push_indent indent_level l;
         go ()
     | (Case | Let) when indent_level < current ->
-        prerr_endline "Case when indent_level < current";
         close_until indent_level next_token
-    | Case | Let ->
-        prerr_endline "Case";
-        token lexbuf
+    | Case | Let -> token lexbuf
     | Case_arm_expr when indent_level = current ->
-        prerr_endline "Case_arm_expr";
         ignore @@ Stack.pop state.stack;
         DEDENT
     | Case_arm_expr when indent_level < current ->
-        prerr_endline "Case_arm_expr when indent_level < current";
         close_until indent_level next_token
-    | Case_arm_expr ->
-        prerr_endline "Case_arm_expr";
-        token lexbuf
-    | Let_def when indent_level < current ->
-        prerr_endline "Let_def when indent_level < current";
-        close_until indent_level next_token
-    | Let_def ->
-        prerr_endline "Let_def";
-        token lexbuf
-    | Let_inline when indent_level < current ->
-        prerr_endline "Let_inline when indent_level < current";
-        DEDENT
-    | Let_inline ->
-        prerr_endline "Let_inline";
-        token lexbuf
+    | Case_arm_expr -> token lexbuf
+    | Let_def when indent_level < current -> close_until indent_level next_token
+    | Let_def -> token lexbuf
+    | Let_inline when indent_level < current -> DEDENT
+    | Let_inline -> token lexbuf
   in
   match (state.prev_token, Stack.top_opt state.stack) with
   | Some LET, Some (_, Let_inline) ->
-      prerr_endline "let multiline";
       let i, _ = Stack.pop state.stack in
       push_indent i Let;
       INDENT
   | _ -> go ()
 
 let handle_equal lexbuf =
-  prerr_endline "handle_equal";
   if get_current_context () = Top_level then (
-    prerr_endline "handle_equal, Top_level branch";
     push_indent 1 Expression;
     Queue.add INDENT state.queue);
   if get_current_context () = Let_def then Queue.add INDENT state.queue;
@@ -243,33 +125,19 @@ let handle_equal lexbuf =
   EQUAL
 
 let handle_case_of lexbuf =
-  prerr_endline "handle_case_of";
   push_indent (get_current_indent ()) Case;
   Queue.add INDENT state.queue;
   OF
 
 let handle_arrow lexbuf =
-  prerr_endline "handle_arrow";
   if get_current_context () = Case then (
     push_indent (get_current_indent ()) Case_arm_expr;
     Queue.add INDENT state.queue);
   ARROW
 
 let next_token token lexbuf =
-  prerr_endline
-  @@ Printf.sprintf "next_token, lexeme: %s"
-  @@ Lexing.lexeme lexbuf;
   let token =
-    if Queue.is_empty state.queue then (
-      let result = token lexbuf in
-      prerr_endline
-      @@ Printf.sprintf "Queue is empty, result %s" (show_token result);
-      result)
-    else
-      let result = Queue.take state.queue in
-      prerr_endline
-      @@ Printf.sprintf "Queue isn't empty, result %s" (show_token result);
-      result
+    if Queue.is_empty state.queue then token lexbuf else Queue.take state.queue
   in
   state.prev_token <- Some token;
   token
@@ -281,18 +149,13 @@ let handle_let lexbuf =
   LET
 
 let handle_let_def lexbuf =
-  prerr_endline "handle_let_def";
-
   match get_current_context () with
   | Let ->
-      prerr_endline "handle_let_def, let branch";
-
       push_indent
         (lexbuf.Lexing.lex_start_p.pos_cnum - state.current_string_cnum)
         Let_def;
       LCNAME (Lexing.lexeme lexbuf)
   | Let_def when state.ident_compare = 0 ->
-      prerr_endline "Let_def when state.ident_compare = 0";
       ignore @@ Stack.pop state.stack;
       push_indent
         (lexbuf.Lexing.lex_start_p.pos_cnum - state.current_string_cnum + 1)
@@ -308,8 +171,6 @@ let handle_let_def lexbuf =
   | _ -> LCNAME (Lexing.lexeme lexbuf)
 
 let handle_in () =
-  prerr_endline "hanlde_in";
-
   match get_current_context () with
   | Let_inline when state.prev_token = Some DEDENT ->
       ignore @@ Stack.pop state.stack;
