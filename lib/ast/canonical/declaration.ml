bcf9dc357b504d79d680e4cf1c60bea9d9dfb279
jongleb <10594395+jongleb@users.noreply.github.com>
2025-10-28 22:12:35 +0400

    wip

--- lib/ast/canonical/declaration.ml
+++ lib/ast/canonical/declaration.ml
@@ -11,3 +11,23 @@ and body_part = {
   params : string Data.Located.t list;
 }
 [@@deriving show]
+
+let of_frontend_type_part (tp : Frontend.Declaration.type_part) : type_part =
+  {
+    name = tp.Frontend.Declaration.name;
+    type_alias = Typedef.Impl.of_frontend tp.type_alias;
+  }
+
+let of_frontend_body_part (bp : Frontend.Declaration.body_part) : body_part =
+  {
+    name = bp.Frontend.Declaration.name;
+    expr = Data.Located.map Expr.of_frontend bp.expr;
+    params = bp.params;
+  }
+
+let of_frontend (decl : Frontend.Declaration.t) : t =
+  {
+    type_part_data =
+      Option.map of_frontend_type_part decl.Frontend.Declaration.type_part_data;
+    body_part = of_frontend_body_part decl.body_part;
+  }
--- lib/ast/canonical/impl.ml
+++ lib/ast/canonical/impl.ml
@@ -2,17 +2,5 @@ type t = Top_declaration of Declaration.t
 
 let of_frontend = function
   | Frontend.Impl.Top_declaration d ->
-      Top_declaration
-        {
-          type_part_data = None;
-          body_part =
-            {
-              name = d.body_part.name;
-              expr =
-                Data.Located.mk
-                  (Expr.of_frontend d.body_part.expr.thing)
-                  d.body_part.expr.loc;
-              params = d.body_part.params;
-            };
-        }
+      Top_declaration (Declaration.of_frontend d)
   | _ -> failwith ""
